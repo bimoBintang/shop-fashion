3bfe8250206119529d87d058715437d37182eee8
Amcu <yudangblu@gmail.com>
2024-12-19 20:01:03 +0700

    solve yarn.lock

--- src/components/test/index.tsx
+++ src/components/test/index.tsx
@@ -1,26 +1,25 @@
 const Subscribe = () => {
-    return (
-      <section className="subscribe">
-        <div className="container">
-          <div
-            style={{ backgroundImage: "url(/images/subscribe.jpg)" }}
-            className="subscribe__content"
-          >
-            <h4>
-              Subscribe to our newsletter and receive exclusive offers every week
-            </h4>
-  
-            <form className="subscribe__form">
-              <input type="email" placeholder="Email address" />
-              <button type="submit" className="btn btn--rounded btn--yellow">
-                Subscribe
-              </button>
-            </form>
-          </div>
+  return (
+    <section className="subscribe">
+      <div className="container">
+        <div
+          style={{ backgroundImage: "url(/images/subscribe.jpg)" }}
+          className="subscribe__content"
+        >
+          <h4>
+            Subscribe to our newsletter and receive exclusive offers every week
+          </h4>
+
+          <form className="subscribe__form">
+            <input type="email" placeholder="Email address" />
+            <button type="submit" className="btn btn--rounded btn--yellow">
+              Subscribe
+            </button>
+          </form>
         </div>
-      </section>
-    );
-  };
-  
-  export default Subscribe;
-  
+      </div>
+    </section>
+  );
+};
+
+export default Subscribe;
